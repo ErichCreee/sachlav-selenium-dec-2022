5763865b68b66ba46be162f16977ea3f079db465
ErichCreee <aptelenmarhash@gmail.com>
2023-02-09 17:55:48 +0200

    Create Hw_01.java

---
+++ src/test/java/Homwork/Hw_01.java
@@ -0,0 +1,29 @@
+package Homwork;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Hw_01 {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://www.amazon.com/");
+        System.out.println("driver.getCurrentUrl() = " + driver.getCurrentUrl());
+        System.out.println("driver.getTitle() = " + driver.getTitle());
+        Thread.sleep(1000);
+
+        driver.get("https://www.walmart.com/");
+
+        driver.navigate().back();
+        Thread.sleep(500);
+        driver.navigate().forward();
+        System.out.println("driver.getCurrentUrl() = " + driver.getCurrentUrl());
+        System.out.println("driver.getTitle() = " + driver.getTitle());
+
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
